c604b93b7c609f175e3f955d217409c18a62a45e
RasoolBooley <rasool.booley@gmail.com>
2019-03-24 16:16:17 +0200

    added base js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,39 @@
-function handMade  (name, price, size, manu) {
+function Handmade  (name, price, manu, img) {
     this.name = name;
     this.price = price;
-    this.size = size;
     this.manu = manu;
+    this.img = img;
 }
+
+// Handmade.prototype.img = function () {
+//     let img = this.img;
+//     return img;
+// }
+
+// Handmade.prototype.name = function () {
+//     let name = this.name;
+//     return name;
+// }
+
+// Handmade.prototype.price = function () {
+//     let price = this.price;
+//     return price;
+// }
+
+Handmade.prototype.describe = function () {
+    let description = "This is a " + this.name + " gorgeous, unique build for " + this.price + " made by " + this.manu + "."
+    return description;
+}
+
+let sculptedTable = new Handmade("Sculpted", 2500 , "Jesse Abrahams", '<src="img/hand-sculpted-table.jpg">');
+let purse = new Handmade("leather purse", 800 , "Paulo Nicolet", '<src="img/paolo-nicolello-666165-unsplash.jpg" >');
+
+let img = document.getElementById("productOne").innerHTML = sculptedTable.productOne();
+
+
+
+
+// -------- JQuery 
 
 $('img[data-enlargable]').addClass('img-enlargable').click(function(){
     var src = $(this).attr('src');
@@ -12,7 +42,7 @@ $('img[data-enlargable]').addClass('img-enlargable').click(function(){
         backgroundSize: 'contain',
         width:'100%', height:'100%',
         position:'fixed',
-        zIndex:'10000',
+        zIndex:'10',
         top:'0', left:'0',
         cursor: 'zoom-out'
     }).click(function(){
